83b29343467e191fba3f39613cc2f1d795a50a2d
thuannt-se <thuannt.se@gmail.com>
2021-04-24 11:15:15 +0700

    Commit solution for Project 1

---
+++ etl.py
@@ -0,0 +1,101 @@
+import os
+import glob
+import psycopg2
+import psycopg2.extras as extras
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath, *args):
+    # open song file
+    df = pd.read_json(filepath, typ='series')
+    # insert song record
+    song_data = (df["song_id"], df["title"], df["artist_id"], df["year"], df["duration"])
+    artist_data = (df["artist_id"], df["artist_name"], df["artist_location"], df["artist_latitude"], df["artist_longitude"])
+    cur.execute(song_table_insert, song_data)
+    # insert artist record
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath, *args):
+    df = pd.read_json(filepath, lines=True)
+     # filter by NextSong action
+    df_next_song = df[df.page.eq('NextSong')]
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df_next_song['ts'], unit='ms')
+    #  # insert time data records
+    if(args[0] == 1) :
+        #use ts as primary key (Not actually unique but we accept it for this project)
+        time_data = {'start_time': df_next_song['ts'], 'hour': t.dt.hour,
+                     'day': t.dt.day, 'week': t.dt.week,
+                     'month': t.dt.month, 'year': t.dt.year, 'weekday': t.dt.day_name()}
+        time_df = pd.DataFrame(time_data)
+        tuples_time = [tuple(x) for x in time_df.to_numpy()]
+        user_df = pd.DataFrame({'user_id': df_next_song['userId'], 'first_name': df_next_song['firstName'],
+                                'last_name':df_next_song['lastName'], 'gender': df_next_song['gender'], 'level': df_next_song['level']})
+        tuples_user = [tuple(x) for x in user_df.to_numpy()]
+        extras.execute_values(cur, time_table_insert, tuples_time)
+        extras.execute_values(cur, user_table_insert, tuples_user)
+
+    else:
+        #Accept to lose some records that have invalid json
+        cur.execute(function_is_valid_json)
+        cur.execute(temp_table_json_holder, (filepath,))
+        cur.execute(user_table_insert_with_copy)
+        cur.execute(time_table_insert_with_copy)
+
+    for index, row in df.iterrows():
+         # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        if results:
+            print('result', results)
+            print((row.song, row.artist))
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (row['ts'], row['userId'],
+                         row['level'], songid, artistid, row['sessionId'],
+                         row['location'], row['userAgent'])
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+
+    for i, datafile in enumerate(all_files, 1):
+        # mode 1: Batch insert
+        # mode 2: Postgresql copy insert
+        try:
+            func(cur, datafile, 2)
+            conn.commit()
+            print('{}/{} files processed.'.format(i, num_files))
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error occurs: %s" % error)
+            conn.rollback()
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=admin")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,84 @@
+# DROP TABLES
+
+songplay_table_drop = "drop table if exists songplays"
+user_table_drop = "drop table if exists users"
+song_table_drop = "drop table if exists songs"
+artist_table_drop = "drop table if exists artists"
+time_table_drop = "drop table if exists time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""create table if not exists songplays(songplay_id SERIAL PRIMARY KEY, start_time numeric, 
+                        user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)""")
+
+user_table_create = ("""create table if not exists users(user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)""")
+
+song_table_create = ("""create table if not exists songs(song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration numeric)""")
+
+artist_table_create = ("""create table if not exists artists(artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric)""")
+
+time_table_create = ("""create table if not exists times(start_time numeric primary key, hour int, day int, week int, month int, year int, weekday varchar)""")
+
+temp_table_json_holder = ("""create temporary table temp_raw_data(values text) on commit drop;
+                            copy temp_raw_data from %s;""")
+# INSERT RECORDS
+
+songplay_table_insert = ("""insert into songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+                            values(%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING""")
+
+song_table_insert = ("""insert into songs(song_id, title, artist_id, year, duration) values (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING""")
+
+artist_table_insert = ("""insert into artists(artist_id, name, location, latitude, longitude) values (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING""")
+
+user_table_insert = ("""insert into users(user_id, first_name, last_name, gender, level) values %s ON CONFLICT DO NOTHING""")
+
+time_table_insert = ("""insert into times(start_time, hour, day, week, month, year, weekday) values %s ON CONFLICT DO NOTHING""")
+# INSERT RECORDS USING COPY
+
+# Insert user table
+user_table_insert_with_copy = ("""insert into users(user_id, first_name, last_name, gender, level)
+                            select  e->> 'userId' as user_id,
+                                    e->> 'firstName' as first_name,
+                                    e->> 'lastName' as last_name,
+                                    e->> 'gender' as gender,
+                                    e->> 'level' as level
+                            from (
+                                select values::json as e from temp_raw_data where is_valid_json(values)
+                            ) s ON CONFLICT DO NOTHING
+""")
+time_table_insert_with_copy = ("""insert into times(start_time, hour, day, week, month, year, weekday)
+                                        select  (e->> 'ts')::numeric as start_time,
+                                            EXTRACT(HOUR FROM to_timestamp((e->> 'ts')::numeric/1000)) as hour,
+                                            EXTRACT(DAY FROM  to_timestamp((e->> 'ts')::numeric/1000)) as day,
+                                            EXTRACT(WEEK FROM  to_timestamp((e->> 'ts')::numeric/1000)) as week,
+                                            EXTRACT(MONTH FROM  to_timestamp((e->> 'ts')::numeric/1000)) as month,
+                                            EXTRACT(YEAR FROM  to_timestamp((e->> 'ts')::numeric/1000)) as year,
+                                            EXTRACT(ISODOW  FROM  to_timestamp((e->> 'ts')::numeric/1000)) as weekday
+                                    from (
+                                        select values::json as e from temp_raw_data where is_valid_json(values)
+                                    ) s ON CONFLICT DO NOTHING                            
+                                """)
+
+# FIND SONGS
+
+song_select = ("""SELECT s.song_id, ar.artist_id FROM artists ar join songs s on ar.artist_id = s.artist_id
+	                where s.title = %s and ar.name = %s and s.duration = %s""")
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
+# CREATE FUNCTION TO CHECK VALID JSON
+function_is_valid_json = ("""create or replace function is_valid_json(p_json text)
+                                  returns boolean
+                                as
+                                $$
+                                begin
+                                  return (p_json::json is not null);
+                                exception 
+                                  when others then
+                                     return false;  
+                                end;
+                                $$
+                                language plpgsql
+                                immutable;""")
